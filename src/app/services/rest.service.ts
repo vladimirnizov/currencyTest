ec153d9daebe036836ef2ad3cc6dbb5cc3b34b88
vladimirnizov <vladimirnizov@gmail.com>
2018-09-29 11:10:16 +0300

    removed rxjs

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { map } from 'rxjs/operators';
+
 
 const url = 'http://apilayer.net/api/live?access_key=a1ea2d135eee9c07f56f40bc60a28035&currencies=USD,EUR,JPY,GBP,RUB&format=1';
 
@@ -12,12 +12,8 @@ export class RestService {
   constructor(private http: HttpClient) { }
 
   getCurrency(){
-    return this.http.get(url).pipe(
-      map(
-        (response)=>{
-      return response;
-    }
-  ));
+    const promise = this.http.get(url);
+    return promise;
   }
 
 }
